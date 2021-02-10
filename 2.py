fbcca3aeed7ca31130c0c8ec6322ee1042016242
Paulo César <paulocesarcs.dev@gmail.com>
2021-02-10 19:47:15 -0300

    Commit 2

---
+++ 2.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    s = a + b
+    d =  a - b
+    p = a * b
+    print(s)
+    print(d)
+    print(p)
