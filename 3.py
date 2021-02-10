db37436a89d22ba3f2c864daa86e9f565a902b51
Paulo César <paulocesarcs.dev@gmail.com>
2021-02-10 19:53:53 -0300

    Commit 3

---
+++ 3.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    inteiro = a // b
+    flutuante = a / b
+    print(inteiro)
+    print(flutuante)
